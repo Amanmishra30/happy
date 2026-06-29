2f4ed81d0b9799d456886abee2b2d042b2212fd0
Amanmishra30 <amanmishra6962@gmail.com>
2026-06-29 19:37:49 +0530

    nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
 import  { useState } from 'react'
-import { useFormState } from 'react-dom'
+import {nanoid} from "nanoid";
 
 const App = () => {
   const [todos, settodos] = useState([
     { id: 1, title: " kaam ho raha hai  bhau" , iscompleted: false},
   ])
 
-  const [title, settitle] = useState(second);
-  return (
-    <div>
-      <h1> create data </h1>
-      <form >
-<input 
-onChange={(e) => 
-  settitle (e.target.value)
-}
-type ="text"
-placeholder="title"/>
+  const [title, settitle] = useState("");
+    
+ const SubmitHandler = (e) => {
+  e.preventDefault();
+
+  const newtodo = {
+    id: nanoid(),
+    title: title,
+    isComplete:false,
+  };
+
+  let copytodos = [...todos];
+  copytodos.push(newtodo);
+  settodos(copytodos);
+ }
+
+ settitle("");
+};
 
 
-<br/>
-<br/>
+const rendertodos = todos.map((todo) => {
+  return <li key ={todo.id}>{todo.title}</li>
+})
 
-        <input type = "text" placeHolder="title"/>
-        <br/>
-        <input
-         onChange ={(e) => console.log(e.target.checked)}
-        
-         type = "checkBox"  /> completed 
-        <br/><br/>
-        <button >Create  Todo </button>
-      </form>
-    </div>
-  )
-}
+return (
 
-export default App
+  <div>
+
+    <h1>Create Tasks</h1>
+    <form onSubmit = {submitHandler}>
+      <input onChange = {(e) => settitle(e.target.value)}
+        value= {title}
+        type="text"
+        placeholder="title"/>
+     <br/>
+     <br/>
+     <button>create todo</button>
+    </form>
+    <hr/>
+    <h1> pending todos</h1>
+    <ol> {rendertodos}</ol>
+  </div>
+
+);
+
+export default App;
